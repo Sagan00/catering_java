4fc01325857623750631560f3fffe4ceacf16edc
Ako7 <Sadowski123@gmail.com>
2023-11-08 19:02:18 +0100

    dodawanie posiłków |java v 0.22

--- src/main/java/com/example/catering/dto/MealDto.java
+++ src/main/java/com/example/catering/dto/MealDto.java
@@ -1,7 +1,15 @@
 package com.example.catering.dto;
-
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class MealDto {
     private Long id;
     private String Name;
+    private String Picture;
     private Long Cal;
 }
